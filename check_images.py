5a3ba77f6c9a666fc867b9b135b1e9c66b4988b0
Nalini Sharma <nalini.sharma@gmail.com>
2019-08-04 10:03:34 -0700

    Final Completed code 08042019

--- check_images.py
+++ check_images.py
@@ -5,7 +5,7 @@
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
 # PROGRAMMER: Nalini Sharma
 # DATE CREATED: 7/29/2019                                 
-# REVISED DATE: 8/1/2019
+# REVISED DATE: 8/4/2019 (final tested code)
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
 #          summarizes how well the CNN performed on the image classification task. 
@@ -118,7 +118,7 @@ def main():
     #      print_results(results, results_stats, in_arg.arch, True, True)
     # Prints summary results, incorrect classifications of dogs (if requested)
     # and incorrectly classified breeds (if requested)
-    print_results(results, results_stats, None, True, True)
+    print_results(results, results_stats, in_arg.arch, True, True)
     
     # TODO 0: Measure total program runtime by collecting end time
     end_time = time()
